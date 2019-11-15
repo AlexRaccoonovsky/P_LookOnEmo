d82b8374affaf11ab3638df950f28e1d1e971cfe
Alex Raccoonovsky <carams2020@gmail.com>
2019-11-15 19:35:17 +0300

    return AppendToCompTable.m

---
+++ func/AppendToCompTable.m
@@ -0,0 +1,14 @@
+function CompTableAppend=AppendToCompTable(InitCellArray)
+% Function for extract & appending parameters in individual columns
+% PosEmo(Positive Emotions), NegEmo (Negative Emotions) of Complex Table
+
+% String of full path to file
+FullPatch=strcat(InitCellArray{1,1},InitCellArray{1,2});
+% load Complex Table data
+CompTable=load(FullPatch,'CompTable');
+% Evaluating PosEmo 
+CompTable.CompTable.PosEmo=CompTable.CompTable.HP-CompTable.CompTable.OP;
+CompTable.CompTable.NegEmo=CompTable.CompTable.OP-CompTable.CompTable.LP;
+% Result
+CompTableAppend = CompTable.CompTable;
+
