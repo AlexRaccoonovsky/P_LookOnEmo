702339f294628c17a2c65ba47d4f80007aecb03b
Alex Raccoonovsky <carams2020@gmail.com>
2019-12-05 18:29:59 +0300

    Append BodyColorColumn to CompTable

--- func/AppendToCompTable.m
+++ func/AppendToCompTable.m
@@ -17,6 +17,29 @@ CompTable.CompTable.NegEmoRel_PerCent=(CompTable.CompTable.NegEmo*100)./CompTabl
 % Evaluating NegEmo (PerCent) relatively OP
 % Creating column Weekday
 CompTable.CompTable.Weekday=weekday(CompTable.CompTable.Date,'long');
+% Append Column for color of Body (Grey Color for immutabled candles (OP~CP)))
+CompTable.CompTable.BodyColor=BodyColorCalculate(CompTable);
 % Result
 CompTableAppend = CompTable.CompTable;
 
+% Subfunction for calculating BodyColor
+ function BodyColorColumn=BodyColorCalculate(CompTable)
+% Calculating difference (CP-OP)
+DifValue=CompTable.CompTable.CP-CompTable.CompTable.OP;
+% Forming BodyColor column
+BodyColorColumn = FormingBodyColor(DifValue);
+% Transformate BodyColor column
+     function BodyColorColumn=FormingBodyColor(DifValue)
+         GreyDeltaConst=3;
+         SizeOfDifValue=size(DifValue)
+         for CurRow=1:1:SizeOfDifValue(1,1)
+             if abs(DifValue(CurRow))<=GreyDeltaConst
+                BodyColorColumn(CurRow,1)="grey";
+             else
+                 if DifValue(CurRow)>0
+                    BodyColorColumn(CurRow,1)="white";
+                 else
+                    BodyColorColumn(CurRow,1)="black";
+                end
+             end
+         end
