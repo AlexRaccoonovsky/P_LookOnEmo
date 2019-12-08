7407b5681ab922cea869b6ff7ee8950f4bc6fc50
Alex Raccoonovsky <carams2020@gmail.com>
2019-12-09 01:23:01 +0300

    Added pie graph & Legend for PosEmo by Range

--- scripts/FormingIncludeParam.m
+++ scripts/FormingIncludeParam.m
@@ -2,7 +2,7 @@
 %% Flag
 IncludeParam{1,1}='Active';
 %% Including Date in Sample
-IncludeParam{2,1}.StartDate =  [20080604];
+IncludeParam{2,1}.StartDate =  [20180914];
 IncludeParam{2,1}.FinishDate = [20190914];
 %% Including month day in a sample
 IncludeParam{3,1}=[1:31];
@@ -13,8 +13,8 @@ IncludeParam{3,1}=[1:31];
 IncludeParam{4,1}=[2 3 4 5 6];
 %% Including Time in Sample
 IncludeParam{5,1}.StartTime =  ["110000"];
-IncludeParam{5,1}.FinishTime = ["114500"];
+IncludeParam{5,1}.FinishTime = ["120000"];
 %% Definition ranges for plotting (absoluting values).
 %% Start point is included in range
-IncludeParam{6,2}.StartPointsOfRanges=[0 5 7 11 19 26 51 101];
-IncludeParam{6,2}.FinishPointsOfRanges=[5 7 10 18 25 50 100 1000];
+IncludeParam{6,2}.StartPointsOfRanges=[0 8 12 19 26 51 101];
+IncludeParam{6,2}.FinishPointsOfRanges=[8 12 18 25 50 100 1000];
--- scripts/TreatAndPlot.m
+++ scripts/TreatAndPlot.m
@@ -1,21 +1,33 @@
 % Script of treatment & plotting
 % Plotting Candle graph for data from CompTableAppend
 subplot(2,2,1:2)
-highlow(CompTableAppend.HP,CompTableAppend.LP,CompTableAppend.CP,CompTableAppend.OP,'red')
-subplot(2,2,3)
-PlotMatrix=CalcAbsoluteScatter(FilteredSample,IncludeParam{6,2})
-bar(PlotMatrix);
-subplot(2,2,4)
+highlow(CompTableAppend.HP,CompTableAppend.LP,CompTableAppend.CP,CompTableAppend.OP,'red');
+
+ax2=subplot(2,2,3)
+[ScatterByRangePosEmoMatrix,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,IncludeParam{6,2});
+bar(ScatterByRangePosEmoMatrix);
+%legend(ax2,RangesValueLegend);
+
+ax3=subplot(2,2,4)
+QuanByRangePosEmoColumn=ScatterByRangePosEmoMatrix(1:end,1);
+pie(QuanByRangePosEmoColumn);
+legend(ax3,RangesValueLegend);
+
 
 %% Scattering by ranges ()
-function PlotMtrx=CalcAbsoluteScatter(FilteredSample,Ranges)
+function [PlotMtrx,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,Ranges)
 QuantOfRanges=size(Ranges.StartPointsOfRanges);
 PlotMtrx=zeros(QuantOfRanges(1,2),4);
 SizeFilteredSample=size(FilteredSample);
         for NumRange=1:1:QuantOfRanges(1,2)
+            % Testing value PosEmo in Rages 
             LogicalMatrixInRange=FilteredSample.PosEmo>=Ranges.StartPointsOfRanges(NumRange)&...
                                  FilteredSample.PosEmo<Ranges.FinishPointsOfRanges(NumRange);
-            PlotMtrx(NumRange,1)=sum(LogicalMatrixInRange); 
+            % Summary value of PosEmo in Range
+            PlotMtrx(NumRange,1)=sum(LogicalMatrixInRange);
+            % Forming Text Cell Array Legend
+            RangesValueLegend{NumRange,1}=strcat(int2str(Ranges.StartPointsOfRanges(NumRange)),...
+                                                '-',int2str(Ranges.FinishPointsOfRanges(NumRange)));
             % Calculate scattering of BodyColor
             for NumRowInFilteredSample=1:1:SizeFilteredSample(1,1)
                 if LogicalMatrixInRange(NumRowInFilteredSample,1)==1
