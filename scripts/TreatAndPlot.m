d76318b1c293d6363effb2d905915ba2bcbfd452
Alex Raccoonovsky <carams2020@gmail.com>
2019-12-10 22:17:04 +0300

    Added graphs for NegEmo & finished manifest

--- scripts/TreatAndPlot.m
+++ scripts/TreatAndPlot.m
@@ -1,29 +1,45 @@
 % Script of treatment & plotting
 % Plotting Candle graph for data from CompTableAppend
-subplot(2,2,1:2)
+ax1=subplot(3,2,1:2)
 highlow(CompTableAppend.HP,CompTableAppend.LP,CompTableAppend.CP,CompTableAppend.OP,'red');
 
-ax2=subplot(2,2,3)
-[ScatterByRangePosEmoMatrix,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,IncludeParam{6,2});
+ax2=subplot(3,2,3)
+[ScatterByRangePosEmoMatrix,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,IncludeParam{6,2},'PosEmo');
 bar(ScatterByRangePosEmoMatrix);
 %legend(ax2,RangesValueLegend);
 
-ax3=subplot(2,2,4)
+ax3=subplot(3,2,4)
 QuanByRangePosEmoColumn=ScatterByRangePosEmoMatrix(1:end,1);
-pie(QuanByRangePosEmoColumn);
+pie3(QuanByRangePosEmoColumn);
 legend(ax3,RangesValueLegend);
+
+ax4=subplot(3,2,5)
+[ScatterByRangeNegEmoMatrix,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,IncludeParam{6,2},'NegEmo');
+bar(ScatterByRangeNegEmoMatrix);
+
+ax5=subplot(3,2,6)
+QuanByRangeNegEmoColumn=ScatterByRangeNegEmoMatrix(1:end,1);
+pie3(QuanByRangeNegEmoColumn);
+legend(ax5,RangesValueLegend);
 
 
 %% Scattering by ranges ()
-function [PlotMtrx,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,Ranges)
+function [PlotMtrx,RangesValueLegend]=CalcAbsoluteScatter(FilteredSample,Ranges,TypeOfEmo)
 QuantOfRanges=size(Ranges.StartPointsOfRanges);
 PlotMtrx=zeros(QuantOfRanges(1,2),4);
 SizeFilteredSample=size(FilteredSample);
+    % Definite comparing column of Emo
+    if (TypeOfEmo=='PosEmo')
+        CompColumn=FilteredSample.PosEmo;
+    else
+        CompColumn=FilteredSample.NegEmo;
+    end
+    
         for NumRange=1:1:QuantOfRanges(1,2)
-            % Testing value PosEmo in Rages 
-            LogicalMatrixInRange=FilteredSample.PosEmo>=Ranges.StartPointsOfRanges(NumRange)&...
-                                 FilteredSample.PosEmo<Ranges.FinishPointsOfRanges(NumRange);
-            % Summary value of PosEmo in Range
+            % Testing value PosEmo in Ranges 
+            LogicalMatrixInRange=CompColumn>=Ranges.StartPointsOfRanges(NumRange)&...
+                                 CompColumn<Ranges.FinishPointsOfRanges(NumRange);
+            % Summary value of emotions in Range
             PlotMtrx(NumRange,1)=sum(LogicalMatrixInRange);
             % Forming Text Cell Array Legend
             RangesValueLegend{NumRange,1}=strcat(int2str(Ranges.StartPointsOfRanges(NumRange)),...
